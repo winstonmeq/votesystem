d2c4755ebf183329a08a32e4654809745eb0dbca
winstonmeq <winstonmequila@gmail.com>
2023-05-31 13:08:25 +0800

    Update Navigator

--- app/page.js
+++ app/page.js
@@ -3,7 +3,8 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className='text=lg'>Vote System</div>
+      <div className='text=lg'>V Monitoring System</div>
+      
     </main>
   )
 }
--- components/Headers.js
+++ components/Headers.js
@@ -6,7 +6,7 @@ import React from 'react'
 const Headers = () => {
   return (
     <div className='flex w-full gap-3 m-5'>
-    <Link href='/'>Headers</Link>
+    <Link href='/'>Home</Link>
     <Link href={'/users'}>Users</Link>
     </div>
   )
