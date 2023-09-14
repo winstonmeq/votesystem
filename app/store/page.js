befe9dd592fdf48b59c277997925dce8875983df
winstonmeq <winstonmequila@gmail.com>
2023-09-14 15:53:27 +0800

    update admin access

--- app/store/page.js
+++ app/store/page.js
@@ -24,24 +24,26 @@ const Page = () => {
 
 
 
-  // useEffect(() => {
+  useEffect(() => {
         
-  //   const fetchDataAndCheckAdmin = async () => {
-  //     try {
-  //       const session = await getSession();
-  //       if (!session || !session.user || !session.user.isAdmin) {
-  //         router.push('/');
-  //       } else {
-  //         console.log('successfully logged in');
-  //         fetchData(); // Fetch data after admin check
-  //       }
-  //     } catch (error) {
-  //       console.error('Error checking admin privileges:', error);
-  //     }  
-  //   };
+    // const fetchDataAndCheckAdmin = async () => {
+    //   try {
+    //     const session = await getSession();
+    //     if (!session || !session.user || !session.user.isAdmin) {
+    //       router.push('/');
+    //     } else {
+    //       console.log('successfully logged in');
+    //       fetchData(); // Fetch data after admin check
+    //     }
+    //   } catch (error) {
+    //     console.error('Error checking admin privileges:', error);
+    //   }  
+    // };
 
-  //   fetchDataAndCheckAdmin();
-  //   }, [router]);
+    // fetchDataAndCheckAdmin();
+
+    fetchData();
+    }, [router]);
   
 
 
