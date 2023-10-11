9d3144415dd5b0c837470be09209cd88bd764b9b
winstonmeq <winstonmequila@gmail.com>
2023-10-11 17:59:37 +0800

    update nextconfig and tile name

--- app/layout.js
+++ app/layout.js
@@ -8,8 +8,8 @@ import Provider from '@/components/Provider'
 // const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'VMS',
-  description: 'Vote Monitoring System',
+  title: 'Alisto+',
+  description: 'Group or Members Monitoring System using mobile app and qrcode system',
 }
 
 export default function RootLayout({ children }) {
