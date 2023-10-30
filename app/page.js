6570f8f4d9c43b412013bc2ac3abb712178bbab7
Winston D. Mequila <winstonmequila@gmail.com>
2023-10-30 20:43:23 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,34 +1,34 @@
-// 'use client'
+'use client'
 
-// import Image from 'next/image'
-// import { useEffect } from 'react';
-// import { useRef } from 'react';
-
-
-// export default function Home() {
+import Image from 'next/image'
+import { useEffect } from 'react';
+import { useRef } from 'react';
 
 
-
-//   return (
-//     <section className='w-full flex-center flex-col'>
+export default function Home() {
 
 
-//     <h1 className='head_text text-center'>
-//     Count with Confidence
-//     </h1>
 
-//       <span className='head_text green_gradient text-center m-2'>
-//       Transforming Members into Trusted Results</span>
+  return (
+    <section className='w-full flex-center flex-col'>
+
+
+    <h1 className='head_text text-center'>
+    Count with Confidence
+    </h1>
+
+      <span className='head_text green_gradient text-center m-2'>
+      Transforming Members into Trusted Results</span>
     
 
-//     <p className='desc text-center mt-2'>
-//     The unwavering assurance in the accuracy and integrity of our system paves the way for a true reflection of the members will
-//     </p>
+    <p className='desc text-center mt-2'>
+    The unwavering assurance in the accuracy and integrity of our system paves the way for a true reflection of the members will
+    </p>
 
    
      
-//     </section>
+    </section>
    
-//   )
-// }
+  )
+}
 
